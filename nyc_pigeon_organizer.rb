fd6289859b3d9d8b4532664c541769cae90b673c
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 06:45:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,58 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attributes, hash|
+    hash.each do |attribute, names|
+      names.each do |name|
+        new_hash[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end
+  data.each do |attribute, hash|
+    hash.each do |value, arr_of_names|
+      new_hash.each do |name, empty|
+        if arr_of_names.include?(name)
+          new_hash[name][attribute].push(value.to_s)
+        end 
+      end
+    end 
+  end
+  new_hash
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+nyc_pigeon_organizer({
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+})
